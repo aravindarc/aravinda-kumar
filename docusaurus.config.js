3bb86bcaa33aa48daa696fd02e8999b0fb421480
aravindarc <aravindrckumar@gmail.com>
2022-03-19 16:38:59 +0530

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Aravinda Kumar',
+  tagline: 'Portfolio and Blog',
+  url: 'https://aravinda-kumar.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'aravindarc', // Usually your GitHub org/user name.
+  projectName: 'aravinda-kumar', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Aravinda Kumar',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/keyboard.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/aravindarc',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Connect with Me',
+            items: [
+              {
+                label: 'Linkedin',
+                href: 'https://www.linkedin.com/in/aravinda-kumar',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/aravindarc',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Aravinda Kumar. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--dark', styles.heroBanner)}>
+      <div className="container">
+        <div className="row" style={{alignItems: "center", justifyContent: "center"}}>
+          <div className="avatar avatar--vertical">
+            <img
+              className="avatar__photo avatar__photo--xl"
+              src="/img/keyboard.svg"
+            />
+            <div className="avatar__intro">
+              <div className="avatar__name">{siteConfig.title}</div>
+              <small className="avatar__subtitle">
+                Fullstack Developer and Cloud Engineer at Bootlabstech
+              </small>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Go to Tutorials
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
