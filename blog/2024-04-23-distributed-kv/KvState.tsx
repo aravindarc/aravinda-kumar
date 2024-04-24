06ea8de67c511cf375d7c3462c5d8d9eeb8c1c1e
aravindarc <aravindrckumar@gmail.com>
2024-04-24 08:40:33 +0530

    kv (2)

--- blog/2024-04-23-distributed-kv/KvState.tsx
+++ blog/2024-04-23-distributed-kv/KvState.tsx
@@ -1,6 +1,7 @@
-import {Box, Button, Grid, IconButton, Input, Paper, TextField} from "@mui/material";
+import {Box, Button, Grid, IconButton, Input, Paper, TextField, Tooltip} from "@mui/material";
 import React from "react";
 import RefreshIcon from '@mui/icons-material/Refresh';
+import PlusOneIcon from '@mui/icons-material/PlusOne';
 
 const host = "https://demokv.aravinda-kumar.com";
 
@@ -20,6 +21,12 @@ async function putKey(key: string, value: string) {
     body: JSON.stringify({
       value: value
     })
+  });
+}
+
+async function addNode() {
+  await fetch(`${host}/kv/add`, {
+    method: 'PUT'
   });
 }
 
@@ -47,7 +54,17 @@ function Node(props: NodeProps) {
     </Paper>
   )
 }
-export default function KvState() {
+
+export enum DemoType {
+  Replication = 1,
+  Scaling,
+}
+
+interface KvStateProps {
+  demoType: DemoType
+}
+
+export function KvState(props: KvStateProps) {
   const [data, setData] = React.useState<Array<Record<string, string>>>([]);
   const [key, setKey] = React.useState<string>("");
   const [value, setValue] = React.useState<string>("");
@@ -60,26 +77,52 @@ export default function KvState() {
   return (
     <Paper sx={{ width: '100%', backgroundColor: 'lightgrey', padding: 2 }}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1}} >
-        <Grid item xs={4} >
-          <TextField color="success" placeholder="Key" sx={{ width: "100%", margin: "0 10px 10px 0"}} value={key} onChange={(e) => setKey(e.target.value)} />
-        </Grid>
-        <Grid item xs={4}>
-          <TextField color="success" placeholder="Value" sx={{  width: "100%", margin: "0 10px 10px 0"}} value={value} onChange={(e) => setValue(e.target.value)} />
-        </Grid>
-        <Grid item xs={3}>
-          <Button color="success" variant="contained" sx={{ width: "100%", height: "85%" }} onClick={
-            () => {
-              putKey(key, value).then(() => {
-                getData().then((data) => {
-                  setData(data);
-                  setKey("");
-                  setValue("");
-                });
-              });
-            }} disabled={key === "" || value === ""}>
-            Add
-          </Button>
-        </Grid>
+        {
+          props.demoType === DemoType.Replication && (
+            <>
+              <Grid item xs={4} >
+                <TextField color="success" placeholder="Key" sx={{ width: "100%", margin: "0 10px 10px 0"}} value={key} onChange={(e) => setKey(e.target.value)} />
+              </Grid>
+              <Grid item xs={4}>
+                <TextField color="success" placeholder="Value" sx={{  width: "100%", margin: "0 10px 10px 0"}} value={value} onChange={(e) => setValue(e.target.value)} />
+              </Grid>
+              <Grid item xs={3}>
+                <Button color="success" variant="contained" sx={{ width: "100%", height: "85%" }} onClick={
+                  () => {
+                    putKey(key, value).then(() => {
+                      getData().then((data) => {
+                        setData(data);
+                        setKey("");
+                        setValue("");
+                      });
+                    });
+                  }} disabled={key === "" || value === ""}>
+                  SET
+                </Button>
+              </Grid>
+            </>
+          )
+        }
+        {
+          props.demoType === DemoType.Scaling && (
+            <>
+              <Grid item xs={10} />
+              <Grid item xs={1}>
+                <IconButton sx={{ width: "100%", height: "85%" }} onClick={
+                  () => {
+                    addNode().then(() => {
+                      setData([...data, {}]);
+                    });
+                  }
+                }>
+                  <Tooltip title={"Add Node"}>
+                    <PlusOneIcon color="success"/>
+                  </Tooltip>
+                </IconButton>
+              </Grid>
+            </>
+          )
+        }
         <Grid item xs={1}>
           <IconButton sx={{ width: "100%", height: "85%" }} onClick={
             () => {
@@ -88,18 +131,20 @@ export default function KvState() {
               });
             }
           }>
-            <RefreshIcon/>
+            <Tooltip title={"Refresh Cluster State"}>
+              <RefreshIcon color="info"/>
+            </Tooltip>
           </IconButton>
         </Grid>
-      </Grid>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {
-          data.map((nodeData, index) => (
-            <Grid item xs={3} key={index}>
-              <Node data={nodeData} id={index}/>
-            </Grid>
-          ))
-        }
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {
+            data.map((nodeData, index) => (
+              <Grid item xs={3} key={index}>
+                <Node data={nodeData} id={index}/>
+              </Grid>
+            ))
+          }
+        </Grid>
       </Grid>
     </Paper>
   );
